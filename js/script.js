6da4c845a1ec2dcc72122e056b80be4b59d3ff7c
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-11-06 23:30:57 +0600

    counter application complite

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 // get fontEnd data 
+const setData = document.getElementById("setData");
 const increment = document.getElementById("increment");
 const decrement = document.getElementById("decrement");
-const setData = document.getElementById("setData");
 
+
+// initial state
+
+let count = 0;
+
+// function to increment the count
+function increments() {
+    count++;
+    setData.innerText = count;
+
+}
+
+// function to decrement the count
+function decrements() {
+    count--;
+    setData.innerText = count;
+    if (count === 0) {
+        decrement.style.display = "none";
+    } else {
+        decrement.style.display = "block";
+    }
+}
+
+// add EventListener 
+increment.addEventListener("click", increments)
+decrement.addEventListener("click", decrements)
