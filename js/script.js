a5980bf0ccd43183dd7c48d3639ee83757021802
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-11-06 22:26:18 +0600

    fint end data get success

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// get fontEnd data 
+const increment = document.getElementById("increment");
+const decrement = document.getElementById("decrement");
+const setData = document.getElementById("setData");
+
